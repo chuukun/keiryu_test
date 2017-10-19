bb378e4856aabaf32bbfdf01196060942acc8114
HAYASHIKEN_W10\hayashiken <hayashi@tera-box.jp>
2017-10-19 15:59:06 +0900

    シーン[keiryu_02]を追加 [scene_01]のプレイ時、マウス移動でカメラが回転するようにしました。

---
+++ CameraMoveTest/Assets/Scenes/Scene_01/Scripts/MouseCameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCameraRotation : MonoBehaviour {
+	
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 2.0F;
+	private float axisX = 0f;
+	private float axisY = 0f;
+
+	void Update() {
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+		float v = verticalSpeed * -Input.GetAxis("Mouse Y");
+		axisY += h;
+		axisX += v;
+		Quaternion rot = Quaternion.AngleAxis (axisY, Vector3.up) * Quaternion.AngleAxis (axisX, Vector3.right);
+		transform.localRotation = rot;
+	}
+}
